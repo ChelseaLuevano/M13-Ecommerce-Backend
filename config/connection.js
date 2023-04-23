bad88e0ebb18c5f6c1b8bf28c68a2513ca2458df
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-23 15:16:37 -0400

    added in all the columns with data requirements for the category, product, product tag, and tag.js files within models folder

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// Enable access to .env variables
+require('dotenv').config();
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
