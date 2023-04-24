f3f24c13435d140d83346640377fa9d4ba19d680
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-23 20:04:08 -0400

    wrote our router.post and router.get by id for category routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,13 +32,24 @@ router.get('/:id', async (req, res) => {
     const categoryData = await Category.findbyPk(req.params.id, {
       include: [{model: Product, through: 'product_category', as:'products' }]
     });
+
+  if (!categoryData) {
+    res.status(404).json({message: 'No category found with this ID.'})
+  }
+
+  res.status(200).json(categoryData);  
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
