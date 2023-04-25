180bb7dc23a3ef21d72a999665e7c8bb459f25c6
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-25 10:52:50 -0400

    completed all the routes within tag-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -102,13 +102,12 @@ router.delete('/:id', async (req, res) => {
       where: {
         id: req.params.id
       }
-    })
+    });
 
   if (!productData) {
     res.status(404).json({message: 'No product found with this id.'})
     return;
   }
-
   res.status(200).json(productData);  
   } catch (err) {
     res.status(500).json(err);
